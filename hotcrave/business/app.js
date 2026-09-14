caaa463e5a1054a3285fe333c2a4adda5f325fed
teamjsdev <teamjsdev@gmail.com>
2026-09-14 13:25:51 -0300

    Fix formatting of API key in Firebase config

--- hotcrave/business/app.js
+++ hotcrave/business/app.js
@@ -4,7 +4,7 @@ import { getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut } from
 const API_BASE = "https://hotcrave-api-staging-274560140811.southamerica-east1.run.app";
 const BUSINESS_WEB_BASE = "https://jsdevmobile.com/hotcrave/b/";
 const FIREBASE_CONFIG = {
-  apiKey: "AIzaSyBG67zAGYRofpCxu02oRKFjPjD_v1HHiOrM",
+  apiKey: "AIzaSyBG67zAGYRofpCxu02oRKfPjD_v1HHiOrM",
   authDomain: "hotcrave-app.firebaseapp.com",
   projectId: "hotcrave-app",
   appId: "1:274560140811:web:841b72c8b3c8a0fae4e90e",
